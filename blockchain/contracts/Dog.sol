f82e74516432a1b464fb38bfe41072ccec1bbf24
Víctor Gracia Magallón <info@victorgraciaweb.com>
2024-03-25 16:48:07 +0200

    Smart Contracts changes

--- blockchain/contracts/Dog.sol
+++ blockchain/contracts/Dog.sol
@@ -5,20 +5,35 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract Dog is ERC721 {
-
     address public owner;
 
-    constructor (string memory _name, string memory _symbol)
-    ERC721(_name, _symbol){
+    constructor(
+        string memory _name,
+        string memory _symbol
+    ) ERC721(_name, _symbol) {
         owner = msg.sender;
     }
 
-    function registerDog(string memory _uuid, string memory _name, string memory _breed, string memory _color, bool availableForAdpt) public {
+    function registerDog(
+        string memory _uuid,
+        string memory _name,
+        string memory _breed,
+        string memory _color,
+        bool _availableForAdpt
+    ) public {
         require(msg.sender == owner, "Only the owner can call this function");
         uint256 newItemId = _tokensIds.current();
-        DogInfo memory dog = DogInfo(newItemId, _uuid, _name, _breed, _color, availableForAdpt);
+        DogInfo memory dog = DogInfo(
+            newItemId,
+            _uuid,
+            _name,
+            _breed,
+            _color,
+            _availableForAdpt
+        );
         dogs.push(dog);
         _safeMint(msg.sender, newItemId);
+        _tokensIds.increment();
     }
 
     // Counters NFTs
@@ -42,7 +57,7 @@ contract Dog is ERC721 {
         return dogs.length;
     }
 
-   function findDog(string memory _uuid) public view returns (DogInfo memory) {
+    function findDog(string memory _uuid) public view returns (DogInfo memory) {
         for (uint256 i = 0; i < dogs.length; i++) {
             if (keccak256(bytes(dogs[i].uuid)) == keccak256(bytes(_uuid))) {
                 return dogs[i];
